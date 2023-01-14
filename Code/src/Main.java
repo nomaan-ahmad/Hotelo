db837d0b9bb47527ef0d17de3452ad8b34a64eb8
Numaan <nomaan.ahmad21@gmail.com>
2023-01-15 01:28:25 +0530

    SignIn feature added

--- Code/src/Main.java
+++ Code/src/Main.java
@@ -1,25 +1,32 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Welcome to HOTELO!.....");
-		
-		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-			System.out.println("Tell me your first name");
+	private static boolean signIn() {
+		boolean flag = false;
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			System.out.println("Every detail is mandatory to fill..");
+			System.out.println("What is your first name ?");
 			String firstName = br.readLine();
-			System.out.println("Tell me your last name");
+			System.out.println("What is your last name ?");
 			String lastName = br.readLine();
-			System.out.println("Tell me your email");
+			System.out.println("Your email address ?");
 			String email = br.readLine();
-			System.out.println("Tell me your mobile number");
+			System.out.println("Your mobile number ?");
 			String mobile = br.readLine();
 			
 			Customer customer = new Customer(firstName, lastName, email, mobile);
-			if (customer.addCustomer()) System.out.println("customer added successfully");
-			else System.out.println("Something unexpected happened");
-		} catch (IOException e) {
+			flag = customer.addCustomer();
+			if (flag) System.out.println("Customer added successfully...");
+			else System.out.println("Something unexpected happened...");
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		return flag;
 	}
-
+	public static void main(String[] args) {
+		System.out.println("Welcome to HOTELO!.....");
+		
+		System.out.println("\n-------------------------------------\n");
+		
+	}
 }
