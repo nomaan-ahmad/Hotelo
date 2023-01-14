5e3a91605c27bdd306e2278302ac0b39d012264c
Numaan <nomaan.ahmad21@gmail.com>
2023-01-14 20:33:26 +0530

    some files added before starting building project

---
+++ Code/src/Customer.java
@@ -0,0 +1,33 @@
+
+public class Customer {
+	private static int id;
+	private String fName;
+	private String lName;
+	private String email;
+	private String mobileNo;
+	
+	public Customer(String _fName, String _lName, String _email, String _mobileNo) {
+		fName = _fName;
+		lName = _lName;
+		email = _email;
+		mobileNo = _mobileNo;
+	}
+	
+	public String getName() {
+		return fName + " " + lName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	
+	public boolean addCustomer(){
+		boolean flag = false;
+		
+		return flag;
+	}
+}
---
+++ Resources/dataset/readme.md
@@ -0,0 +1,6 @@
+<h1>Dataset</h1>
+<p>
+    Dataset is taken from Kaggle : Link 
+    <a href ="https://www.kaggle.com/datasets/PromptCloudHQ/hotels-on-makemytrip?resource=download"></a>
+</p>
+<p>I haven't used all of the attributes but only some of them</p>
